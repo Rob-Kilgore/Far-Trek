09aa3de107aa05d9b598cc6ebca9bf164ed6fdbc
Daniel <danielmaryanski@gmail.com>
2019-12-06 21:48:58 -0500

    Added temp getPath query functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import ReactMapGL from "react-map-gl";
+import URL from 'url';
 import './App.css';
+import {getPath} from './navigation/Navigate';
 
 function App() {
 	const [viewport, setViewport] = useState({
@@ -11,6 +13,20 @@ function App() {
 		height: "100vh",		
 	});
 	
+	// Parse URL
+	const queryParams = URL.parse(window.location.href, true).query;
+	const startPosition = queryParams.startPosition;
+	const endPosition = queryParams.endPosition;
+	const weight = queryParams.weight;
+	
+	var path = [];
+	if(startPosition && endPosition && weight){
+		path = getPath(startPosition, endPosition, weight/100.0);
+		console.log(path);
+	}
+	
+	
+	
 	return (
 		<div>			
 			<ReactMapGL {...viewport}
@@ -19,10 +35,18 @@ function App() {
 				onViewportChange={viewport => {
 					setViewport(viewport);
 				}}
-			>		
-			</ReactMapGL>
+			/>
 			<div style={{position: "absolute", top: "0px", left: "0px"}}>
-				<input type="button"></input>
+			<form>				
+				Start Position: <input type="text" name="startPosition" value={startPosition}/>
+				<br />
+				End Position: <input type="text" name="endPosition" value={endPosition}/>
+				<br />
+				Weight: <input type="range" name="weight" min="0" max="100" step="1" value={weight}/>
+				<br />
+				<input type="submit" value="Calculate Path" />
+			</form>
+			
 			</div>
 		</div>
 	);
