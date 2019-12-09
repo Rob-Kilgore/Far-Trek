5829964510a1be9e8d4017e53603d8ebd229a562
Jacob Calkins <jcalkins@umass.edu>
2019-12-09 13:33:08 -0500

    Fixed the renderer

--- src/components/nav/route/Renderer.js
+++ src/components/nav/route/Renderer.js
@@ -8,64 +8,55 @@ import {PathLayer} from '@deck.gl/layers';
 import { render } from '@testing-library/react';
 import switch_icon from './switch_icon.svg';
 
-const viewport = useState({
-	latitude: 42.391484,
-	longitude: -72.529089,
-	zoom: 10,
-	width: "100vw",
-	height: "100vh",		
-});
-
-class Renderer extends React.Component {
-	constructor(props) {
-		super(props);
-
-		let path = useState([]);
-		let setPath = useState([]);
+function Renderer() {
+	const [viewport, setViewport] = useState({
+		latitude: 42.391484,
+		longitude: -72.529089,
+		zoom: 10,
+		width: "100vw",
+		height: "100vh",		
+	});
+	var [path, setPath] = useState([]);
 	
-		let startPosition = "";
-		let endPosition = "";
-		let weight = 0;
-	}
-
-	updatePathLayer(){
-		if(this.startPosition && this.endPosition){
-			this.setPath(getPath(this.startPosition, this.endPosition, this.weight));
+	var startPosition = "";
+	var endPosition = "";
+	var weight = 0;
+		
+	var pathLayer = new PathLayer({
+		id: 'path-layer',
+		data: [{path: path}],
+		pickable: true,
+		widthScale: 20,
+		widthMinPixels: 2,
+		getPath: d => d.path,
+		getColor: d => [255, 140, 0],
+		getWidth: d => 1,
+		rounded: true,		
+	});
+	
+	var updatePathLayer = function(){
+		if(startPosition && endPosition){
+			setPath(getPath(startPosition, endPosition, weight));
 		}		
-	}
-
-	render() {
-
-		const pathLayer = new PathLayer({
-			id: 'path-layer',
-			data: [{path: this.path}],
-			pickable: true,
-			widthScale: 20,
-			widthMinPixels: 2,
-			getPath: d => d.path,
-			getColor: d => [255, 140, 0],
-			getWidth: d => 1,
-			rounded: true,		
-		});
-
-		return (
-			<div className="RenderMap">			
-				<NavInput />
-				<DeckGL
-					initialViewState={this.viewport}
-					height="100%"
-					width="100%"
-					controller={true} // allows the user to move the map around
-					layers={[this.pathLayer]}
-				>
-					<StaticMap
-						mapStyle="mapbox://styles/fartrekker/ck3ut11zx2cgj1cs4uz06ahu2"
-						mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-					/>
-				</DeckGL>
-			</div>
-		);
-	}
+	}	
+		
+	return (
+		<div>			
+			<DeckGL
+				initialViewState={viewport}
+				height="100%"
+				width="100%"
+				controller={true} // allows the user to move the map around
+				layers={[pathLayer]}
+			>
+				<StaticMap
+					mapStyle="mapbox://styles/fartrekker/ck3ut11zx2cgj1cs4uz06ahu2"
+					mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+				/>
+			</DeckGL>
+			<NavInput />
+		</div>
+	);
 }
 
 
@@ -73,28 +64,17 @@ class NavInput extends React.Component {
 	render() {
 		return(
 			<div className="NavInput">
-				<div style={{position: "absolute", top: "0px", left: "0px"}}>
-				<form>				
-					Start Position:
-				<input type="text" name="startPosition"
-					onChange={(event) => {this.startPosition = event.target.value}}
-				/>
-				<br />
-				End Position:
-				<input type="text" name="endPosition"
-					onChange={(event) => {this.endPosition = event.target.value}}
-				/>
-				<br />
-				Weight:
-				<input type="range" name="weight"
-					min="0" max="100" step="1"
-					onChange={(event) => {this.weight = event.target.value/100.0}}
-				/>
-				<br />
-				<input type="button" value="Calculate Path"
-					onClick={this.updatePathLayer}
-				/>
-				</form>
+				<div id="input-box">
+					<form>
+						<input class="nav-input-field" type="text" name="startPosition"  placeholder="start" onChange={(event) => {this.startPosition = event.target.value}} />
+						<input class="nav-input-field" type="text" name="endPosition"  placeholder="destination" onChange={(event) => {this.endPosition = event.target.value}} />
+						<div id="weight-wrapper">
+							<span class="weight-labels">easier</span>
+							<input class="slider" type="range" name="weight" min="0" max="100" step="1" onChange={(event) => {this.weight = event.target.value/100.0}} />
+							<span class="weight-labels">harder</span>
+						</div>
+						<input id="navigate-btn" type="button" value="navigate" onClick={this.updatePathLayer} />
+					</form>
 				</div>
 			</div>
 		);
