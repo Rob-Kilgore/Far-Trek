5e47a9a893bd33e89773ba3959811df4f20aec05
Jacob Calkins <jcalkins@umass.edu>
2019-12-09 15:06:38 -0500

    Added a fixed searchbar

--- src/components/search/Searchbar.js
+++ src/components/search/Searchbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import styles from './Searchbar.css';
-import search_icon from './search_icon.svg';
+import './Searchbar.css';
+import search_icon from './search_icon.png';
 
 class Searchbar extends React.Component {
   render() {
     return(
       <div className="Searchbar">
-        <form class={styles.searchbar} action="">
-          <input class={styles.searchbar} type="text" name="searchbar" placeholder="search for a destination..." maxlength="100"/>
-          <button class={styles.searchbar} type="submit"><img src={search_icon} /></button>
+        <form class="searchbar" action="">
+          <input id="search-box" type="text" name="searchbar" placeholder="search for a destination..." maxlength="100"/>
+          <button id="search-btn" type="submit"><img src={search_icon} alt="search icon" /></button>
         </form>
       </div>
     );
